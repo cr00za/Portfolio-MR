23c7bb40e2adf18d89b73102a67bb1a0cdb7d92b
Miguel Ruddock <miguel.uxd@gmail.com>
2020-01-08 18:25:06 -0500

    FIxed Smooth Scroll
    - Fixed logo color on scroll mobile
    - Remaining issue: logo color is white when mobile scroll is 0 (top of page) ---need it to be dark

--- js/interactions.js
+++ js/interactions.js
@@ -1,47 +1,46 @@
 
-//  when doc is ready do this function
+// Smooth Scroll
+// Select all »a« elements with a parent class »links« and add a function that is executed on click
+$( '.links a' ).on( 'click', function(e){
+	
+  // Define variable of the clicked »a« element (»this«) and get its href value.
+  var href = $(this).attr( 'href' );
+  
+  // Run a scroll animation to the position of the element which has the same id like the href value.
+  $( 'html, body' ).animate({
+		scrollTop: $( href ).offset().top
+  }, '300' );
+	
+  // Prevent the browser from showing the attribute name of the clicked link in the address bar
+  e.preventDefault();
 
-
+});
+  
 
 // The scroll() method triggers the scroll event, or attaches a function to run when a scroll event occurs.
 // scrollTop returns the scroll position
-    
-
 //when window scrolls do this function
 $(document).scroll(function(){
     var scroll = $(window).scrollTop();
     if(scroll>45) {
-        $(".navbar-custom").css("background", "rgba(2, 50, 70, 0.85)");
+        $(".navbar-custom").css("background", "rgba(2, 50, 70, 1)");
         $(".navbar-nav a").css("color", "#F6F6F6");
+        $("nav").removeClass("navbar-light").addClass("navbar-dark");
+        
     }
     else {
         $(".navbar-custom").css("background", "transparent");
         $(".navbar-nav a").css("color", "#023246");
+        $("nav").removeClass("navbar-dark").addClass("navbar-light");   
+        
     }
+
 });
 
 
-$(document).ready(function() {
-    $('.navbar-nav a').click(function(e) {
-  
-      var targetHref = $(this).attr('href');
-  
-      $('html, body').animate({
-        scrollTop: $(targetHref).offset().top
-      }, 1000);
-  
-      e.preventDefault();
-    });
-  });
 
-// Smooth JS Anchor Scroll for normal # (make sure to include id="" in your links and not just a name="")
-//Anchor Animation
-// jQuery('a').click(function(){
-//     jQuery('html, body').animate({
-//         scrollTop: jQuery( jQuery(this).attr('href') ).offset().top
-//     }, 500);
-//     return false;
-// }); 
+
+
 
 // Trying to change the logo to dark version on screen size <768px
 // $(document).ready(function(){
@@ -51,43 +50,6 @@ $(document).ready(function() {
 //         $(".heroImageBlock").css("padding-bottom", "30%");
 //     } 
 // });
-// Jquery Smooth scroll
-//Anchor Animation
-// jQuery('.nav-link').click(function(){
-//     jQuery('html, body').animate({
-//         scrollTop: jQuery( jQuery(this).attr('href') ).offset().top
-//     }, 500);
-//     return false;
-// }); 
 
-// $(document).ready(function(){
-//     if (screen.width < 980) {
-//         $(".heroImageBlock").css("padding-bottom", "30%");
-//     }
-// }
-
-
-// $(document).ready(function(){
-//     $('.heroBlock').popover({
-//     trigger: "hover",
-//         delay: {show : 3000, hide : 0} });
-// });
-
-
-
-// smooth scroll to anchor, with option of hash appearing in url. 
-// (e) is the event handler
-// $(document).ready(function(){
-// 	$('a[href^="#"]').on('click',function (e) {
-// 	    e.preventDefault();
-// 	    var target = this.hash;
-// 	    var $target = $(target);
-// 	    $('html, body').stop().animate({
-// 	        'scrollTop': $target.offset().top
-// 	    }, 900, 'swing', function () {
-// 	        // window.location.hash = target;
-// 	    });
-// 	});
-// });
 
 
