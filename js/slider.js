c16db0d9f4000895a653453a107af473e0cf9ec9
Miguel Ruddock <miguel.uxd@gmail.com>
2020-01-13 21:48:32 -0500

    Better form styling
    - added close button
    - working on adding submission text

--- js/slider.js
+++ js/slider.js
@@ -6,8 +6,21 @@ Function to activate the contact form to open
 */
 
 $(document).ready(function(){
-    $('.toggle').click(function(){
+    $('.toggle, .formClose').click(function(){
       $('.sidebar-contact').toggleClass('active')
       $('.toggle').toggleClass('active')
     })
-  })
+
+  });
+
+
+
+  $(document).ready(function(){
+    $('.formSubmit').click(function(){
+        $(".formResponse").show();
+        $('.formBlanket').css("background-color", "#023246");
+    })
+});
+//   if ($('.formSubmit').hasClass('active' {
+//     display: "show";
+// }
