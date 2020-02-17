872b15c5744172797706899dec11e5e065ed9069
cr00za <52474968+cr00za@users.noreply.github.com>
2020-02-17 10:02:56 -0500

    clean up jumbostron
    clean up jumbostron - after Holly's instruction about color

--- js/interactions.js
+++ js/interactions.js
@@ -54,7 +54,10 @@ $(document).scroll(function(){
 
 });
 
-
+$( '#topheader .navbar-nav a' ).on( 'click', function () {
+	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
 
 
 
