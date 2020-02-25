270f2ac2178e616d6f0ad4657e57c4b88ac7dc35
cr00za <52474968+cr00za@users.noreply.github.com>
2020-02-25 02:32:59 -0500

    fixed hover to activate glasses color to change
    - fixed hover to activate glasses color to change

--- js/hero.js
+++ js/hero.js
@@ -5,27 +5,27 @@
 
 // Change glasses color on hover of different elements
 $(document).ready(function(){
-    $(".glassesProcess").hover(function() {
+    $(".glassesPortfolio, .portfolioButton").hover(function() {
     $( '.glasses' ).attr("src","images/Portfolio Portrait-sky.svg");
     });
 
-    $(".toggle, .glassesPortfolio, .glassesHome, .glassesAbout").hover(function() {
+    $(".glassesHome, .myleft-side, .glasses, .heroImageBlock, .contactButton").hover(function() {
         $( '.glasses' ).attr("src","images/Portfolio Portrait-blue.svg");
         
     });
 
-    $(".heroBlock, .heroImageBlock").hover(function() {
+    $(".glassesAbout, .myright-side, .glassesProcess, .heroBlock").hover(function() {
         $( '.glasses' ).attr("src","images/Portfolio Portrait-navy.svg");
     });
+
+
+});
 
     // switching active class when clicking on a navigation item
     $('a.js-anchor-link').click(function(){
         $('a.js-anchor-link').removeClass('active');
         $(this).addClass('active');
     });
-});
-
-
 
 // $(".qa-faq-title").click( function(){
 //     $(".qa-faq-title.active").removeClass("active");
