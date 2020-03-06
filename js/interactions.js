930fe268b3470093d38b0e4a3532671ccca1f1fa
cr00za <52474968+cr00za@users.noreply.github.com>
2020-03-06 00:16:19 -0500

    Page load optimization
    - reduced portfolio picture sizes to reduce page load time

--- js/interactions.js
+++ js/interactions.js
@@ -27,12 +27,6 @@ $( '.links a' ).on( 'click', function(e){
   e.preventDefault();
 });
   
-// Fading Text into focus
-// $(document).ready(function(){
-//   AOS.init({
-//     duration: 1200,
-//   })
-// });
 
 $('.navbar-collapse a').click(function (e) {
     $('.show').collapse('toggle');
@@ -47,7 +41,7 @@ $(document).scroll(function(){
     
    
 
-    if(scroll>20) {
+    if(scroll>2) {
         $(".navbar-custom").css("background", "rgba(246, 246, 246, 1)");
         $(".navbar-light .navbar-nav .nav-link.active").css("color", "#023246");
         $(".navbar-nav a").css("color", "#023246");
@@ -67,67 +61,4 @@ $(document).scroll(function(){
 
 
 });
-
-
-
-// $(document).ready(function () {
-//   $(document).on("scroll", onScroll);
-  
-//   //smoothscroll
-//   $('a[href^="#"]').on('click', function (e) {
-//       e.preventDefault();
-//       $(document).off("scroll");
-      
-//       $('a').each(function () {
-//           $(this).removeClass('active');
-//       })
-//       $(this).addClass('active');
-    
-//       var target = this.hash,
-//           menu = target;
-//       $target = $(target);
-//       $('html, body').stop().animate({
-//           'scrollTop': $target.offset().top+2
-//       }, 500, 'swing', function () {
-//           window.location.hash = target;
-//           $(document).on("scroll", onScroll);
-//       });
-//   });
-// });
-
-// function onScroll(event){
-//   var scrollPos = $(document).scrollTop();
-//   $('#menu-center a').each(function () {
-//       var currLink = $(this);
-//       var refElement = $(currLink.attr("href"));
-//       if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-//           $('#menu-center ul li a').removeClass("active");
-//           currLink.addClass("active");
-//       }
-//       else{
-//           currLink.removeClass("active");
-//       }
-//   });
-// }
-
-
-// $( '#topheader .navbar-nav a' ).on( 'click', function () {
-// 	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-// 	$( this ).parent( 'li' ).addClass( 'active' );
-// });
-
-
-
-
-
-// Trying to change the logo to dark version on screen size <768px
-// $(document).ready(function(){
-//     if( $(window).width() <= 768 ){
-//         // do your stuff
-//         $("#logo").attr("src", "images/logo-dark.svg");
-//         $(".heroImageBlock").css("padding-bottom", "30%");
-//     } 
-// });
-
-
 
