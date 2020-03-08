07ba82770e635802359a4a2c6ac6da3c9c73b5e7
cr00za <52474968+cr00za@users.noreply.github.com>
2020-03-07 23:02:02 -0500

    changed images back to jpg
     - images were not showing up on mobile

--- js/interactions.js
+++ js/interactions.js
@@ -62,3 +62,38 @@ $(document).scroll(function(){
 
 });
 
+/*-------------------------------*/
+/***** HERO ANIMATION        *****/
+/*-------------------------------*/
+
+
+// Change glasses color on hover of different elements
+$(document).ready(function(){
+    
+
+  $(".glassesPortfolio, .portfolioButton").hover(function() {
+  $( '.glasses' ).attr("src","images/Portfolio Portrait-sky.svg");
+  });
+
+  $(".glassesAbout, .glassesProcess").hover(function() {
+      $( '.glasses' ).attr("src","images/Portfolio Portrait-blue.svg");
+  });
+
+  $(".glassesHome, .glasses, myright-side .heroImageBlock, .contactButton, .jumbotron").hover(function() {
+      $( '.glasses' ).attr("src","images/Portfolio Portrait-navy.svg");
+  });
+});
+
+
+
+  // switching active class when clicking on a navigation item
+  $('a.js-anchor-link').click(function(){
+      $('a.js-anchor-link').removeClass('active');
+      $(this).addClass('active');
+  });
+
+// $(".qa-faq-title").click( function(){
+//     $(".qa-faq-title.active").removeClass("active");
+
+//     $(this).addClass("active");
+// });
